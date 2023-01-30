24ae434b99f282a642013b16b5fce006ccb08b47
Ju$er <36333877+JuSerbotnet39@users.noreply.github.com>
2023-01-30 17:58:06 +0530

    chatGPT got it wrong had to correct it.

--- passwd_gen.cpp
+++ passwd_gen.cpp
@@ -2,35 +2,32 @@
 #include <string>
 #include <random>
 
-std::string generatePassword(int length, std::string charset)
+std::string generatePassword(int length, std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 {
-  std::string password = "";
   std::mt19937 generator(std::random_device{}());
-  std::uniform_int_distribution<int> distribution(0, charset.size() - 1);
+  std::uniform_int_distribution<int> distribution(0, charset.length() - 1);
+
+  std::string password;
   for (int i = 0; i < length; i++)
   {
     password += charset[distribution(generator)];
   }
+
   return password;
 }
 
 int main()
 {
-  int passwordLength = 16;
-  std::string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#@";
-  std::cout &#8203;
-  `oaicite: { "index" : 0, "invalid_reason" : "Malformed citation << \"Enter the password length (minimum 6): \";\n  std::cin >>" }
-  `&#8203;
-  passwordLength;
-  if (passwordLength < 6)
-  {
-    std::cout << "Error: password length must be at least 6." << std::endl;
-    return 1;
-  }
-  std::cout &#8203;
-  `oaicite: { "index" : 1, "invalid_reason" : "Malformed citation << \"Enter the characters to be included in the password: \";\n  std::cin >>" }
-  `&#8203;
-  charset;
-  std::cout << generatePassword(passwordLength, charset) << std::endl;
+  int length;
+  std::cout<<"Enter the password length (minimum 6): \n";    
+  std::cin>>length;
+
+  std::string charset;
+  std::cout << "Enter a custom character set (optional): ";
+  std::cin.ignore();
+  std::getline(std::cin, charset);
+
+  std::cout << "Generated password: " << generatePassword(length, charset.empty() ? "" : charset) << std::endl;
+
   return 0;
 }
